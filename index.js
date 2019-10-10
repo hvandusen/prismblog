a91f471b5d97419650468d6273ce8111566ac11c
hvandusen <henry@dusendusen.com>
2019-10-10 01:44:26 -0400

    oops

--- index.js
+++ index.js
@@ -1,72 +1,64 @@
-var fs = require('fs');
-var _ = require('underscore');
-var mongoose = require('mongoose');
-var Datauri = require('datauri');
-var svg2png = require('svg2png')
-//mongoose.connect('mongodb://henry:henryvd1@ds059702.mongolab.com:59702/candusenhub');
-mongoose.connect('mongodb://localhost/candusenhub');
-var db = mongoose.connection;
-var Blogshape = require('./models/blogshape');
-var paper = require('paper');
-var ejs = require('ejs');
-var express = require('express')
-var twilio = require('twilio')
+var jsdom = require('jsdom')
+const { JSDOM } = jsdom
+var fs = require("fs")
+var paper = require("paper")
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://hvandusen:"+process.env.MONGO_PW+"@hankscluster-dtp7l.mongodb.net/test?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true })
+const express = require('express');
+const nunjucks = require('nunjucks');
+let svgs = []
+
+updateSvgs();
+
 var app = express();
-app.use(express.static(__dirname + '/public'));
-var server = app.listen( 3033 );
-var schedule = require('node-schedule');
-app.set('view engine','ejs')
-var posts = [];
-app.get('/', function (request, response) {
-    response.render('index',{posts: svgs});
-});
-var svgs;
-var scope;
-var twil_client = require('twilio')('ACcd6586f5cbe5bbbb2759e3349c0f0603', '5c12f398f18492d6cd20e0bd62c769e3');
 
-
-Blogshape.find(function(err,docs){
-  svgs = docs;
-});
-db.on('error', console.error.bind(console, 'connection error:'));
-//
-everyDay();
-
-var j = schedule.scheduleJob('* * * * *', function(){
-    everyDay()
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
 });
 
-function everyDay(){
-  scope = require('./prism2.pjs')(new paper.Size(800, 700));
-  pic = new Blogshape({
-    svg: scope.project.exportSVG({'asString':true}),
-    date: Date().split(' ').splice(0,4).join(' ')
-  });
-  pic.save(function(err,docs){
-    Blogshape.find(function(err,docs){
-      svgs = docs;
-    });
-    svg2png('output.svg','new.png',function(err){
-        Datauri('new.png', function (err, content) {
-          if (err) {
-            throw err;
-          }
-          twil_client.sendMessage({
-              to:'+12024927472', // Any number Twilio can deliver to
-              from: '+12015913520', // A number you bought from Twilio and can use for outbound communication
-              body: 'does this god damn work ;(.', // body of the SMS message
-              MediaUrl: content
-          }, function(err, responseData) { //this function is executed when a response is received from Twilio
-              if (!err) { // "err" is an error received during the request, if any
-                  console.log(responseData.from); // outputs "+14506667788"
-                  console.log(responseData.body); // outputs "word to your mother."
-              }
-        });
+app.get('/', function(req, res) {
+    res.render('index.html',{data: svgs});
+});
+
+app.listen(process.env.PORT ? process.env.PORT : 3332);
+
+function getNewShape(){
+  const dom = new JSDOM('<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title></title></head><body></body></html>')
+  var {execute} = paper.PaperScript;
+  var c = dom.window.document.createElement("canvas")
+  c.width = 1000
+  c.height = 1000
+  paper.setup(c)
+  paper.activate()
+  var text = fs.readFileSync("./prism2.pjs").toString()
+  execute(text,paper)
+  return paper.project.exportSVG({
+    asString: true,
+    matchShapes: true
+  })
+}
+
+
+
+function updateSvgs(){
+  client.connect(err => {
+    if(err){
+      console.log(err)
+    }
+    const collection = client.db("prism-blog").collection("shapes")
+    // fs.writeFile("texst.svg",getNewShape());
+      collection.insert({
+        svg: getNewShape(),
+        date: new Date()
       })
-        })
-
-
-
+    collection.find().sort({date: -1}).toArray()
+    .then( data => {
+      svgs = [ ...data ];
+      console.log("there are this many shapes :) ",svgs.length)
+      client.close()
+    })
 
 
   });
